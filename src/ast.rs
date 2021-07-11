5dd92d41cbd551cfc42fd43a25c5deb6d4cf4f3c
gidra5 <hranitel.52@gmail.com>
2021-07-11 15:50:29 +0300

    update

--- src/ast.rs
+++ src/ast.rs
@@ -60,8 +60,7 @@ pub struct ASTNodeExt<'a, T> {
 pub enum Expression {
   BinaryExpression(
     Option<Box<Expression>>,
-    Option<Token>,
-    // Option<Value>,
+    Option<Value>,
     Option<Box<Expression>>,
   ),
 }
@@ -74,19 +73,10 @@ pub enum Fixity {
   None,
 }
 
-#[derive(PartialEq, Eq, Hash, Debug, Clone)]
-pub struct Operator {
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub struct Operator<'a> {
   fixity: Fixity,
-  token:  Option<Token>,
-}
-
-impl Default for Operator {
-  fn default() -> Operator {
-    Operator {
-      fixity: Fixity::None,
-      token:  Some(Token::LParenthesis),
-    }
-  }
+  token:  Option<TokenExt<'a>>,
 }
 
 impl Display for Expression {
@@ -99,290 +89,124 @@ impl Display for Expression {
       },
       Self::BinaryExpression(None, op, Some(right)) => write!(f, "({:?} {})", op, right),
     }
-    //   match self {
-    //     Self::BinaryExpression(Some(left), Some(op), Some(right)) => {
-    //       write!(f, "({:?} {} {})", op, left, right)
-    //     },
-    //     Self::BinaryExpression(Some(left), None, Some(right)) => {
-    //       write!(f, "(FnCall {} {})", left, right)
-    //     },
-    //     Self::BinaryExpression(None, Some(op), Some(right)) => {
-    //       write!(f, "({:?}| {})", op, right)
-    //     },
-    //     Self::BinaryExpression(None, Some(op), None) => {
-    //       write!(f, "{:?}", op)
-    //     },
-    //     Self::BinaryExpression(Some(left), Some(op), None) => {
-    //       write!(f, "(|{:?} {})", op, left)
-    //     },
-    //     Self::BinaryExpression(left, None, right) => write!(f, "(FnCall {:?}
-    // {:?})", left, right),     Self::BinaryExpression(left, op, right) =>
-    // {       write!(f, "({:?} {:?} {:?})", op, left, right)
-    //     },
-    //     Self::Literal(value) => write!(f, "{:?}", value),
-    //   }
   }
 }
 
-/// return true if op_left < op_right
-fn compare(
-  Operator {
-    fixity: fixity_left,
-    token: token1,
-  }: &Operator,
-  Operator {
-    fixity: fixity_right,
-    token: token2,
-  }: &Operator,
-) -> Option<bool> {
-  use Token::*;
-
-  println!(
-    "{:?}, {:?}",
-    Operator {
-      fixity: fixity_left.clone(),
-      token:  token1.clone(),
-    },
-    Operator {
-      fixity: fixity_right.clone(),
-      token:  token2.clone(),
-    }
-  );
-
-  let token_left = token1.as_ref()?;
-  let token_right = token2.as_ref()?;
-
-  Some(match (fixity_left, token_left, fixity_right, token_right) {
-    // (_, _, Fixity::Postfix, RParenthesis) => true,
-    (Fixity::Prefix, Add, fixity_left, token_left)
-    | (Fixity::Prefix, Sub, fixity_left, token_left) => {
-      match (fixity_right, token_right) {
-        (Fixity::Infix, Add) | (Fixity::Infix, Sub) => false,
-        _ => true,
-      }
-    },
-    (Fixity::None, LParenthesis, _, _) | (Fixity::Prefix, LParenthesis, _, _) => true,
-    (Fixity::Infix, Add, fixity_right, token_right)
-    | (Fixity::Infix, Sub, fixity_right, token_right) => {
-      match (fixity_right, token_right) {
-        (Fixity::Infix, Add) | (Fixity::Infix, Sub) => false,
-        _ => true,
-      }
-    },
-    _ => return None,
-  })
+impl Expression {
+  pub fn parse(token_stream: &mut TokenStream<'_>) -> Result<Expression, &'static str> {
+    expr_bp(token_stream)
+  }
 }
 
-// fn binding_power(op: char, prefix: bool) -> Option<(u8, u8)> {
-//   let res = match op {
-//       '=' => (2, 1),
-//       '+' | '-' if prefix => (99, 9),
-//       '+' | '-' => (5, 6),
-//       '*' | '/' => (7, 8),
-//       '!' => (11, 100),
-//       '.' => (14, 13),
-//       _ => return None,
-//   };
-//   Some(res)
-// }
 
-// fn prefix_binding_power(op: char) -> ((), u8) {
-//   match op {
-//       '+' | '-' => ((), 9),
-//       _ => panic!("bad op: {:?}", op),
-//   }
-// }
+#[derive(Debug)]
+struct Frame<'a> {
+  operator: Option<Operator<'a>>,
+  min_bp:   u8,
+  lhs:      Option<Expression>,
+}
 
-// fn postfix_binding_power(op: char) -> Option<(u8, ())> {
-//   let res = match op {
-//       '!' => (11, ()),
-//       '[' => (11, ()),
-//       _ => return None,
-//   };
-//   Some(res)
-// }
+fn expr_bp(lexer: &mut TokenStream) -> Result<Expression, &'static str> {
+  let mut top = Frame {
+    min_bp:   0,
+    operator: None,
+    lhs:      None,
+  };
+  let mut stack = Vec::new();
 
-// fn infix_binding_power(op: char) -> Option<(u8, u8)> {
-//   let res = match op {
-//       '=' => (2, 1),
-//       '?' => (4, 3),
-//       '+' | '-' => (5, 6),
-//       '*' | '/' => (7, 8),
-//       '.' => (14, 13),
-//       _ => return None,
-//   };
-//   Some(res)
-// }
+  loop {
+    let token = lexer.next();
 
-impl Expression {
-  // fn parse2(
-  //   token_stream: &mut TokenStream<'_>,
-  //   parent_op: Operator,
-  // ) -> Result<Expression, &'static str> {
-  //   use Token::*;
+    let (operator, r_bp) = loop {
+      match binding_power2(Operator {
+        token:  token.clone(),
+        fixity: if top.lhs.is_none() {
+          Fixity::Prefix
+        } else {
+          Fixity::Infix
+        },
+      }) {
+        Some((t, (l_bp, r_bp))) if top.min_bp <= l_bp => break (t, r_bp),
+        _ => {
+          let res = top;
+          top = match stack.pop() {
+            Some(it) => it,
+            None => return res.lhs.ok_or("No Expression"),
+          };
 
-  //   // let mut lhs = Err("no expr");
+          top.lhs = Some(Expression::BinaryExpression(
+            top.lhs.map(Box::new),
+            res
+              .operator
+              .clone()
+              .map(|op| op.token.map(|op| op.value()))
+              .flatten(),
+            res.lhs.map(Box::new),
+          ))
+        },
+      };
+    };
 
-  //   // loop {
-  //   //   let token = match token_stream.peek() {
-  //   //     Some(t) => t,
-  //   //     None => return lhs,
-  //   //   };
-
-  //   //   let op = Operator {
-  //   //     fixity: if lhs.is_err() {
-  //   //       Fixity::Prefix
-  //   //     } else {
-  //   //       Fixity::Infix
-  //   //     },
-  //   //     token:  Some(token.clone().token),
-  //   //   };
-  //   //   if compare(&parent_op, &op).unwrap_or(true) {
-  //   //     return lhs;
-  //   //   }
-
-  //   //   token_stream.next();
-
-  //   //   let rhs = Self::parse2(token_stream, op)?;
-
-  //   //   lhs = Ok(Self::BinaryExpression(
-  //   //     lhs.ok().map(|x| Box::new(x)),
-  //   //     Some(token.token),
-  //   //     Some(Box::new(rhs)),
-  //   //   ))
-  //   // }
-
-  //   //   fn expr_bp(lexer: &mut Lexer, min_bp: u8) -> Option<S> {
-  //   //     let mut lhs = None;
-
-  //   //     loop {
-  //   //         let token = match lexer.peek() {
-  //   //             Some(token) => token,
-  //   //             None => return lhs,
-  //   //         };
-
-  //   //         let r_bp = match binding_power(token, lhs.is_none()) {
-  //   //             Some((l_bp, r_bp)) if min_bp <= l_bp => r_bp,
-  //   //             _ => return lhs,
-  //   //         };
-
-  //   //         lexer.next();
-
-  //   //         let rhs = expr_bp(lexer, r_bp);
-  //   //         if token == '(' {
-  //   //             assert_eq!(lexer.next(), Some(')'));
-  //   //             lhs = rhs;
-  //   //             continue;
-  //   //         }
-
-  //   //         let mut args = Vec::new();
-  //   //         args.extend(lhs);
-  //   //         args.extend(rhs);
-  //   //         lhs = Some(S::Cons(token, args));
-  //   //     }
-  //   // }
-
-  //   let mut lhs = match token_stream.peek() {
-  //     Some(TokenExt { token, .. }) => {
-  //       match token {
-  //         Identifier | Number | String | Char | Boolean => {
-  //           // Ok(Expression::Literal(token_stream.next().unwrap().value()))
-  //           Err("")
-  //         },
-  //         LParenthesis => {
-  //           token_stream.next();
-  //           let lhs = Self::parse2(token_stream, Operator::default())?;
-  //           if let Some(TokenExt {
-  //             token: RParenthesis,
-  //             ..
-  //           }) = token_stream.next()
-  //           {
-  //             Ok(lhs)
-  //           } else {
-  //             return Err("no closing parenthesis");
-  //           }
-  //         },
-  //         _ => Err("no expr"),
-  //       }
-  //     },
-  //     None => Err("no expr"),
-  //   };
-
-  //   loop {
-  //     let token = match token_stream.peek() {
-  //       None => break,
-  //       Some(TokenExt { token, .. }) => token,
-  //     };
-
-  //     // let op = Operator {
-  //     //   fixity: Fixity::Postfix,
-  //     //   token:  Some(token.clone()),
-  //     // };
-  //     // if let Some(true) = compare(&parent_op, &op) {
-  //     //   token_stream.next();
-  //     //   lhs = Expression::BinaryExpression(Some(Box::new(lhs)), Some(token),
-  // None);     //   continue;
-  //     // }
-
-  //     let op = Operator {
-  //       fixity: if lhs.is_err() {
-  //         Fixity::Prefix
-  //       } else {
-  //         Fixity::Infix
-  //       },
-  //       token:  Some(token.clone()),
-  //     };
-  //     // if let None = compare(&parent_op, &op)
-
-  //     if let Some(true) = compare(&parent_op, &op) {
-  //       token_stream.next();
-  //       let rhs = Self::parse2(token_stream, op)?;
-
-  //       lhs = Ok(Expression::BinaryExpression(
-  //         lhs.ok().map(Box::new),
-  //         Some(token),
-  //         Some(Box::new(rhs)),
-  //       ));
-  //       continue;
-  //     }
-
-  //     break;
-  //   }
-
-  //   lhs
-  // }
-
-  pub fn parse(token_stream: &mut TokenStream<'_>) -> Result<Expression, &'static str> {
-    // Self::parse2(token_stream, Operator::default())
-    match expr_bp(token_stream) {
-      Some(e) => Ok(e),
-      None => Err(""),
+    if let Some(TokenExt {
+      token: Token::RParenthesis,
+      ..
+    }) = operator.token
+    {
+      if let Some(Operator {
+        token: Some(TokenExt {
+          token: Token::LParenthesis,
+          ..
+        }),
+        ..
+      }) = top.operator
+      {
+        let res = top;
+        top = stack.pop().unwrap();
+        top.lhs = res.lhs;
+        continue;
+      } else {
+        return Err("Unexpected closing parenthesis");
+      }
+    } else if let Some(Operator {
+      token: Some(TokenExt {
+        token: Token::LParenthesis,
+        ..
+      }),
+      ..
+    }) = top.operator
+    {
+      return Err("Expected closing parenthesis");
     }
+
+    stack.push(top);
+    top = Frame {
+      operator: Some(operator),
+      min_bp:   r_bp,
+      lhs:      None,
+    };
   }
 }
 
 
 // #[derive(Debug)]
-// struct Frame {
-//   operator: Option<Operator>,
-//   min_bp:   u8,
-//   lhs:      Option<Expression>,
-//   // token:    Option<TokenExt<'a>>,
+// struct Frame<'a> {
+//   min_bp: u8,
+//   lhs:    Option<Expression>,
+//   token:  Option<TokenExt<'a>>,
 // }
 
 // fn expr_bp(lexer: &mut TokenStream) -> Option<Expression> {
 //   let mut top = Frame {
-//     min_bp:   0,
-//     operator: None,
-//     lhs:      None,
-//     // token:    None,
+//     min_bp: 0,
+//     lhs:    None,
+//     token:  None,
 //   };
 //   let mut stack = Vec::new();
 
 //   loop {
 //     let token = lexer.next();
 
-//     let (operator, r_bp) = loop {
+//     let (token, r_bp) = loop {
 //       match binding_power(token.clone(), top.lhs.is_none()) {
 //         Some((t, (l_bp, r_bp))) if top.min_bp <= l_bp => break (t, r_bp),
 //         _ => {
@@ -394,7 +218,7 @@ impl Expression {
 
 //           top.lhs = Some(Expression::BinaryExpression(
 //             top.lhs.map(Box::new),
-//             res.operator.clone().map(|op| op.token).flatten(),
+//             res.token.clone().map(|op| op.value()),
 //             res.lhs.map(Box::new),
 //           ))
 //         },
@@ -414,75 +238,15 @@ impl Expression {
 
 //     stack.push(top);
 //     top = Frame {
-//       operator: Some(operator),
-//       min_bp:   r_bp,
-//       lhs:      None,
-//       // token:    Some(token),
+//       min_bp: r_bp,
+//       lhs:    None,
+//       token:  Some(token),
 //     };
 //   }
 // }
 
-
-#[derive(Debug)]
-struct Frame<'a> {
-  min_bp: u8,
-  lhs:    Option<Expression>,
-  token:  Option<TokenExt<'a>>,
-}
-
-fn expr_bp(lexer: &mut TokenStream) -> Option<Expression> {
-  let mut top = Frame {
-    min_bp: 0,
-    lhs:    None,
-    token:  None,
-  };
-  let mut stack = Vec::new();
-
-  loop {
-    let token = lexer.next();
-
-    let (token, r_bp) = loop {
-      match binding_power(token.clone(), top.lhs.is_none()) {
-        Some((t, (l_bp, r_bp))) if top.min_bp <= l_bp => break (t, r_bp),
-        _ => {
-          let res = top;
-          top = match stack.pop() {
-            Some(it) => it,
-            None => return res.lhs,
-          };
-
-          top.lhs = Some(Expression::BinaryExpression(
-            top.lhs.map(Box::new),
-            res.token.clone().map(|op| op.token),
-            res.lhs.map(Box::new),
-          ))
-        },
-      };
-    };
-
-    if token.token == Token::RParenthesis {
-      assert_eq!(
-        top.token.clone().map(|token| token.token),
-        Some(Token::LParenthesis)
-      );
-      let res = top;
-      top = stack.pop().unwrap();
-      top.lhs = res.lhs;
-      continue;
-    }
-
-    stack.push(top);
-    top = Frame {
-      min_bp: r_bp,
-      lhs:    None,
-      token:  Some(token),
-    };
-  }
-}
-
-fn binding_power2(op: Option<Operator>) -> Option<(Operator, (u8, u8))> {
+fn binding_power2(op: Operator) -> Option<(Operator, (u8, u8))> {
   use Token::*;
-  let op = op?;
   let res = match op.clone() {
     Operator {
       fixity: Fixity::None,
@@ -494,8 +258,11 @@ fn binding_power2(op: Option<Operator>) -> Option<(Operator, (u8, u8))> {
     } => {
       (
         match token? {
-          RParenthesis => 0,
-          Bang => 11,
+          TokenExt {
+            token: RParenthesis,
+            ..
+          } => 0,
+          TokenExt { token: Bang, .. } => 11,
           _ => return None,
         },
         100,
@@ -506,9 +273,19 @@ fn binding_power2(op: Option<Operator>) -> Option<(Operator, (u8, u8))> {
       token,
     } => {
       (99, match token? {
-        LParenthesis => 0,
-        Add | Sub => 9,
-        _ => return None,
+        TokenExt {
+          token: LParenthesis,
+          ..
+        } => 0,
+        TokenExt {
+          token: Add | Sub, ..
+        } => 9,
+        token => {
+          return binding_power2(Operator {
+            fixity: Fixity::None,
+            token:  Some(token),
+          })
+        },
       })
     },
     Operator {
@@ -516,11 +293,20 @@ fn binding_power2(op: Option<Operator>) -> Option<(Operator, (u8, u8))> {
       token,
     } => {
       match token? {
-        Equal => (2, 1),
-        Add | Sub => (5, 6),
-        Mult | Div => (7, 8),
-        Period => (14, 13),
-        _ => return None,
+        TokenExt { token: Equal, .. } => (2, 1),
+        TokenExt {
+          token: Add | Sub, ..
+        } => (5, 6),
+        TokenExt {
+          token: Mult | Div, ..
+        } => (7, 8),
+        TokenExt { token: Period, .. } => (14, 13),
+        token => {
+          return binding_power2(Operator {
+            fixity: Fixity::Postfix,
+            token:  Some(token),
+          })
+        },
       }
     },
     _ => return None,
